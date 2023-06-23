058d0a92c114144e3ac225cc012d71f58de039dc
Bianca Egwu <bianca.egwu9@gmail.com>
2023-06-23 10:19:50 -0400

    Initial Commit

---
+++ AllAboutMeProject/AllAboutMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutMeProjectApp.swift
+//  AllAboutMeProject
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllAboutMeProject/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  AllAboutMeProject
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("All about me ")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+                .multilineTextAlignment(.center)
+                .lineLimit(0)
+                .padding(/*@START_MENU_TOKEN@*/.bottom, 0.0/*@END_MENU_TOKEN@*/)
+                .accessibilityIdentifier(/*@START_MENU_TOKEN@*/"Identifier"/*@END_MENU_TOKEN@*/)
+            
+            Image("Blue")
+                .renderingMode(.original)
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .aspectRatio(contentMode: .fit)
+                .padding(0.0)
+            
+            
+            Button("Click Here for more Facts") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.gray)
+            
+           
+            
+                
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
